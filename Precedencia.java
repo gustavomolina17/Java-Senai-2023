a3aa7be254cb2b1e21f0d43dba8694a964ba2564
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2023-07-18 10:32:31 -0300

    Delete

--- Precedencia.java
+++
@@ -1,9 +0,0 @@
-public class Precedencia {
-
-    public static void main(String[] args) {
-        int calculo = 3*((2+6)/2)%2;
-        System.out.println("Resultado: "+calculo);
-        
-    }
-    
-}
